68a97447471788767c1c5207f801e498ab4383e7
iishiishii <love_sweet_98@yahoo.com.vn>
2024-04-18 10:30:01 +1000

    remove unused import and functions in infer script

--- CVPR24_LiteMedSamOnnx_infer.py
+++ CVPR24_LiteMedSamOnnx_infer.py
@@ -1,16 +1,12 @@
-from os import listdir, makedirs
-from os.path import join, isfile, basename
+from os import makedirs
+from os.path import join, basename
 from glob import glob
 from tqdm import tqdm
 from time import time
 import numpy as np
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 
-from src.litemedsam.modeling import MaskDecoder, PromptEncoder, TwoWayTransformer
-from src.litemedsam.tiny_vit_sam import TinyViT
-from matplotlib import pyplot as plt
 import cv2
 import argparse
 from collections import OrderedDict
@@ -22,7 +18,6 @@ import gc
 #%% set seeds
 torch.set_float32_matmul_precision('high')
 torch.manual_seed(2024)
-torch.cuda.manual_seed(2024)
 np.random.seed(2024)
 
 parser = argparse.ArgumentParser()
@@ -66,25 +61,13 @@ parser.add_argument(
     action='store_true',
     help='whether to save the overlay image'
 )
-parser.add_argument(
-    '-png_save_dir',
-    type=str,
-    default='./overlay',
-    help='directory to save the overlay image'
-)
 
 args = parser.parse_args()
 
 data_root = args.input_dir
 pred_save_dir = args.output_dir
-save_overlay = args.save_overlay
 num_workers = args.num_workers
-if save_overlay:
-    assert args.png_save_dir is not None, "Please specify the directory to save the overlay image"
-    png_save_dir = args.png_save_dir
-    makedirs(png_save_dir, exist_ok=True)
 
-# lite_medsam_checkpoint_path = args.lite_medsam_checkpoint_path
 makedirs(pred_save_dir, exist_ok=True)
 device = torch.device(args.device)
 image_size = 256
@@ -118,113 +101,35 @@ def pad_image(image, target_size=256):
 
     return image_padded
 
-class MedSAM_Lite(nn.Module):
-    def __init__(
-            self, 
-            image_encoder, 
-            mask_decoder,
-            prompt_encoder
-        ):
-        super().__init__()
-        self.image_encoder = image_encoder
-        self.mask_decoder = mask_decoder
-        self.prompt_encoder = prompt_encoder
-
-    def forward(self, image, box_np):
-        image_embedding = self.image_encoder(image) # (B, 256, 64, 64)
-        # do not compute gradients for prompt encoder
-        with torch.no_grad():
-            box_torch = torch.as_tensor(box_np, dtype=torch.float32, device=image.device)
-            if len(box_torch.shape) == 2:
-                box_torch = box_torch[:, None, :] # (B, 1, 4)
-
-        sparse_embeddings, dense_embeddings = self.prompt_encoder(
-            points=None,
-            boxes=box_np,
-            masks=None,
-        )
-        low_res_masks, iou_predictions = self.mask_decoder(
-            image_embeddings=image_embedding, # (B, 256, 64, 64)
-            image_pe=self.prompt_encoder.get_dense_pe(), # (1, 256, 64, 64)
-            sparse_prompt_embeddings=sparse_embeddings, # (B, 2, 256)
-            dense_prompt_embeddings=dense_embeddings, # (B, 256, 64, 64)
-            multimask_output=False,
-          ) # (B, 1, 256, 256)
-
-        return low_res_masks
-
-    @torch.no_grad()
-    def postprocess_masks(self, masks, new_size, original_size):
-        """
-        Do cropping and resizing
-
-        Parameters
-        ----------
-        masks : torch.Tensor
-            masks predicted by the model
-        new_size : tuple
-            the shape of the image after resizing to the longest side of 256
-        original_size : tuple
-            the original shape of the image
-
-        Returns
-        -------
-        torch.Tensor
-            the upsampled mask to the original size
-        """
-        # Crop
-        masks = masks[..., :new_size[0], :new_size[1]]
-        # Resize
-        masks = F.interpolate(
-            masks,
-            size=(original_size[0], original_size[1]),
-            mode="bilinear",
-            align_corners=False,
-        )
-
-        return masks
-
-
-def show_mask(mask, ax, mask_color=None, alpha=0.5):
+def postprocess_masks(masks, new_size, original_size):
     """
-    show mask on the image
+    Do cropping and resizing
 
     Parameters
     ----------
-    mask : numpy.ndarray
-        mask of the image
-    ax : matplotlib.axes.Axes
-        axes to plot the mask
-    mask_color : numpy.ndarray
-        color of the mask
-    alpha : float
-        transparency of the mask
-    """
-    if mask_color is not None:
-        color = np.concatenate([mask_color, np.array([alpha])], axis=0)
-    else:
-        color = np.array([251/255, 252/255, 30/255, alpha])
-    h, w = mask.shape[-2:]
-    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
-    ax.imshow(mask_image)
+    masks : torch.Tensor
+        masks predicted by the model
+    new_size : tuple
+        the shape of the image after resizing to the longest side of 256
+    original_size : tuple
+        the original shape of the image
 
-
-def show_box(box, ax, edgecolor='blue'):
+    Returns
+    -------
+    torch.Tensor
+        the upsampled mask to the original size
     """
-    show bounding box on the image
+    # Crop
+    masks = masks[..., :new_size[0], :new_size[1]]
+    # Resize
+    masks = F.interpolate(
+        masks,
+        size=(original_size[0], original_size[1]),
+        mode="bilinear",
+        align_corners=False,
+    )
 
-    Parameters
-    ----------
-    box : numpy.ndarray
-        bounding box coordinates in the original image
-    ax : matplotlib.axes.Axes
-        axes to plot the bounding box
-    edgecolor : str
-        color of the bounding box
-    """
-    x0, y0 = box[0], box[1]
-    w, h = box[2] - box[0], box[3] - box[1]
-    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor=edgecolor, facecolor=(0,0,0,0), lw=2))     
+    return masks
 
 
 def get_bbox256(mask_256, bbox_shift=3):
@@ -283,45 +188,8 @@ def resize_box_to_256(box, original_size):
 
     return new_box
 
-
 @torch.no_grad()
-def medsam_inference(medsam_model, img_embed, box_256, new_size, original_size):
-    """
-    Perform inference using the LiteMedSAM model.
-
-    Args:
-        medsam_model (MedSAMModel): The MedSAM model.
-        img_embed (torch.Tensor): The image embeddings.
-        box_256 (numpy.ndarray): The bounding box coordinates.
-        new_size (tuple): The new size of the image.
-        original_size (tuple): The original size of the image.
-    Returns:
-        tuple: A tuple containing the segmented image and the intersection over union (IoU) score.
-    """
-    box_torch = torch.as_tensor(box_256[None, None, ...], dtype=torch.float, device=img_embed.device)
-    
-    sparse_embeddings, dense_embeddings = medsam_model.prompt_encoder(
-        points = None,
-        boxes = box_torch,
-        masks = None,
-    )
-    low_res_logits, iou = medsam_model.mask_decoder(
-        image_embeddings=img_embed, # (B, 256, 64, 64)
-        image_pe=medsam_model.prompt_encoder.get_dense_pe(), # (1, 256, 64, 64)
-        sparse_prompt_embeddings=sparse_embeddings, # (B, 2, 256)
-        dense_prompt_embeddings=dense_embeddings, # (B, 256, 64, 64)
-        multimask_output=False
-    )
-
-    low_res_pred = medsam_model.postprocess_masks(low_res_logits, new_size, original_size)
-    low_res_pred = torch.sigmoid(low_res_pred)  
-    low_res_pred = low_res_pred.squeeze().cpu().numpy()  
-    medsam_seg = (low_res_pred > 0.5).astype(np.uint8)
-
-    return medsam_seg, iou
-
-@torch.no_grad()
-def onnx_decoder_inference(medsam_model, image_embedding_slice, input_points, input_box, new_size, original_size):
+def onnx_decoder_inference(decoder_session, image_embedding_slice, input_points, input_box, new_size, original_size):
     # decoder_session = onnxruntime.InferenceSession(decoder_path, providers=['CPUExecutionProvider'])
 
     if len(input_points)>0:
@@ -355,7 +223,7 @@ def onnx_decoder_inference(medsam_model, image_embedding_slice, input_points, in
     masks, scores, low_res_logits = decoder_session.run(None, decoder_inputs)
     # if ori_mask:
     low_res_logits = torch.tensor(low_res_logits)
-    low_res_pred = medsam_model.postprocess_masks(low_res_logits, new_size, original_size)
+    low_res_pred = postprocess_masks(low_res_logits, new_size, original_size)
     low_res_pred = torch.sigmoid(low_res_pred)  
     low_res_pred = low_res_pred.squeeze().cpu().numpy()  
     medsam_seg = (low_res_pred > 0.5).astype(np.uint8)
@@ -363,60 +231,7 @@ def onnx_decoder_inference(medsam_model, image_embedding_slice, input_points, in
         # mask = (masks > 0).astype(np.uint8)
     return medsam_seg, scores
 
-medsam_lite_image_encoder = TinyViT(
-    img_size=256,
-    in_chans=3,
-    embed_dims=[
-        64, ## (64, 256, 256)
-        128, ## (128, 128, 128)
-        160, ## (160, 64, 64)
-        320 ## (320, 64, 64) 
-    ],
-    depths=[2, 2, 6, 2],
-    num_heads=[2, 4, 5, 10],
-    window_sizes=[7, 7, 14, 7],
-    mlp_ratio=4.,
-    drop_rate=0.,
-    drop_path_rate=0.0,
-    use_checkpoint=False,
-    mbconv_expand_ratio=4.0,
-    local_conv_size=3,
-    layer_lr_decay=0.8
-)
-# encoder_session = onnxruntime.InferenceSession(lite_medsam_checkpoint_path, providers=['CPUExecutionProvider'])
-
-medsam_lite_prompt_encoder = PromptEncoder(
-    embed_dim=256,
-    image_embedding_size=(64, 64),
-    input_image_size=(256, 256),
-    mask_in_chans=16
-)
-
-medsam_lite_mask_decoder = MaskDecoder(
-    num_multimask_outputs=3,
-        transformer=TwoWayTransformer(
-            depth=2,
-            embedding_dim=256,
-            mlp_dim=2048,
-            num_heads=8,
-        ),
-        transformer_dim=256,
-        iou_head_depth=3,
-        iou_head_hidden_dim=256,
-)
-
-medsam_lite_model = MedSAM_Lite(
-    image_encoder = medsam_lite_image_encoder,
-    mask_decoder = medsam_lite_mask_decoder,
-    prompt_encoder = medsam_lite_prompt_encoder
-)
-
-# lite_medsam_checkpoint = torch.load(lite_medsam_checkpoint_path, map_location='cpu')
-# medsam_lite_model.load_state_dict(lite_medsam_checkpoint)
-# medsam_lite_model.to(device)
-# medsam_lite_model.eval()
-
-def MedSAM_infer_npz_2D(img_npz_file):
+def MedSAM_infer_npz_2D(img_npz_file, encoder_session, decoder_session):
     gc.collect()
 
     npz_name = basename(img_npz_file)
@@ -443,7 +258,7 @@ def MedSAM_infer_npz_2D(img_npz_file):
         box256 = resize_box_to_256(box, original_size=(H, W))
         box256 = box256[None, ...] # (1, 4)
         # medsam_mask, iou_pred = medsam_inference(medsam_lite_model, image_embedding, box256, (newh, neww), (H, W))
-        medsam_mask, iou_pred = onnx_decoder_inference(medsam_lite_model, image_embedding, [], box256, (newh, neww), [H, W])
+        medsam_mask, iou_pred = onnx_decoder_inference(decoder_session, image_embedding, [], box256, (newh, neww), [H, W])
         # print("medsam_mask", medsam_mask.shape, medsam_mask.dtype, segs.shape)
         segs[medsam_mask>0] = idx
         # print(f'{npz_name}, box: {box}, predicted iou: {np.round(iou_pred.item(), 4)}')
@@ -453,28 +268,8 @@ def MedSAM_infer_npz_2D(img_npz_file):
         segs=segs,
     )
 
-    # visualize image, mask and bounding box
-    if save_overlay:
-        fig, ax = plt.subplots(1, 2, figsize=(10, 5))
-        ax[0].imshow(img_3c)
-        ax[1].imshow(img_3c)
-        ax[0].set_title("Image")
-        ax[1].set_title("LiteMedSAM Segmentation")
-        ax[0].axis('off')
-        ax[1].axis('off')
 
-        for i, box in enumerate(boxes):
-            color = np.random.rand(3)
-            box_viz = box
-            show_box(box_viz, ax[1], edgecolor=color)
-            show_mask((segs == i+1).astype(np.uint8), ax[1], mask_color=color)
-
-        plt.tight_layout()
-        plt.savefig(join(png_save_dir, npz_name.split(".")[0] + '.png'), dpi=300)
-        plt.close()
-
-
-def MedSAM_infer_npz_3D(img_npz_file):
+def MedSAM_infer_npz_3D(img_npz_file, encoder_session, decoder_session):
     gc.collect()
 
     npz_name = basename(img_npz_file)
@@ -527,7 +322,7 @@ def MedSAM_infer_npz_3D(img_npz_file):
                 else:
                     box_256 = resize_box_to_256(mid_slice_bbox_2d, original_size=(H, W))
             # img_2d_seg, iou_pred = medsam_inference(medsam_lite_model, image_embedding, box_256, [new_H, new_W], [H, W])
-            img_2d_seg, iou_pred = onnx_decoder_inference(medsam_lite_model, image_embedding, [], box_256, [new_H, new_W], [H, W])
+            img_2d_seg, iou_pred = onnx_decoder_inference(decoder_session, image_embedding, [], box_256, [new_H, new_W], [H, W])
             
             segs_3d_temp[z, img_2d_seg>0] = idx
         
@@ -564,7 +359,7 @@ def MedSAM_infer_npz_3D(img_npz_file):
                 scale_256 = 256 / max(H, W)
                 box_256 = mid_slice_bbox_2d * scale_256
             # img_2d_seg, iou_pred = medsam_inference(medsam_lite_model, image_embedding, box_256, [new_H, new_W], [H, W])
-            img_2d_seg, iou_pred = onnx_decoder_inference(medsam_lite_model, image_embedding, [], box_256, [new_H, new_W], [H, W])
+            img_2d_seg, iou_pred = onnx_decoder_inference(decoder_session, image_embedding, [], box_256, [new_H, new_W], [H, W])
             
             segs_3d_temp[z, img_2d_seg>0] = idx
         segs[segs_3d_temp>0] = idx
@@ -572,30 +367,6 @@ def MedSAM_infer_npz_3D(img_npz_file):
         join(pred_save_dir, npz_name),
         segs=segs,
     )            
-
-    # visualize image, mask and bounding box
-    if save_overlay:
-        idx = int(segs.shape[0] / 2)
-        fig, ax = plt.subplots(1, 2, figsize=(10, 5))
-        ax[0].imshow(img_3D[idx], cmap='gray')
-        ax[1].imshow(img_3D[idx], cmap='gray')
-        ax[0].set_title("Image")
-        ax[1].set_title("LiteMedSAM Segmentation")
-        ax[0].axis('off')
-        ax[1].axis('off')
-
-        for i, box3D in enumerate(boxes_3D, start=1):
-            if np.sum(segs[idx]==i) > 0:
-                color = np.random.rand(3)
-                x_min, y_min, z_min, x_max, y_max, z_max = box3D
-                box_viz = np.array([x_min, y_min, x_max, y_max])
-                show_box(box_viz, ax[1], edgecolor=color)
-                show_mask(segs[idx]==i, ax[1], mask_color=color)
-
-        plt.tight_layout()
-        plt.savefig(join(png_save_dir, npz_name.split(".")[0] + '.png'), dpi=300)
-        plt.close()
-
 
 if __name__ == '__main__':
     img_npz_files = sorted(glob(join(data_root, '*.npz'), recursive=True))
@@ -619,9 +390,9 @@ if __name__ == '__main__':
         start_time = time()
         print('Processing:', img_npz_file)
         if basename(img_npz_file).startswith('3D'):
-            MedSAM_infer_npz_3D(img_npz_file)
+            MedSAM_infer_npz_3D(img_npz_file, encoder_session, decoder_session)
         else:
-            MedSAM_infer_npz_2D(img_npz_file)
+            MedSAM_infer_npz_2D(img_npz_file, encoder_session, decoder_session)
         end_time = time()
         efficiency['case'].append(basename(img_npz_file))
         efficiency['time'].append(end_time - start_time)
