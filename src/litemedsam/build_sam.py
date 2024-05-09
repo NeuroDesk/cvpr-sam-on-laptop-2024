560d95144b73c38b5425f1f0abc0ac41bc007174
yexincheng <ye_xcheng@163.com>
2024-05-09 11:11:48 +1000

    rm model key

--- src/litemedsam/build_sam.py
+++ src/litemedsam/build_sam.py
@@ -155,7 +155,7 @@ def build_sam_vit_t(checkpoint=None):
     mobile_sam.eval()
     if checkpoint is not None:
         with open(checkpoint, "rb") as f:
-            state_dict = torch.load(f, map_location="cpu")['model']
+            state_dict = torch.load(f, map_location="cpu")
         mobile_sam.load_state_dict(state_dict)
     return mobile_sam
 
